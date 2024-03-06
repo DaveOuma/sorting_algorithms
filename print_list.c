4903a681831022649b1e39cd573b7599c63bb477
David Ouma <davecarlosotieno@gmail.com>
2024-03-05 20:14:08 -0500

    Created and tested insertion

---
+++ print_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * print_list - Prints all the elements of a doubly linked list of integers.
+ * @list: Pointer to the head of the list.
+ */
+void print_list(const listint_t *list)
+{
+    const listint_t *current = list;
+
+    while (current != NULL)
+    {
+        printf("%d", current->n);
+        if (current->next != NULL)
+            printf(", ");
+        current = current->next;
+    }
+    printf("\n");
+}
+
